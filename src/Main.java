e403734d7cec0d6a16ca8734325b0d1b1c45ee64
Laboratório Informática <labinfo@unifil.br>
2019-07-31 22:03:42 -0300

    Refatora service de leitura

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Service service = new Service();
-
+        System.out.println("------------------Buffered Reader------------------------");
         service.leituraBufferedReader("textoGrande", "Grande");
         service.leituraBufferedReader("textoMedio", "Medio");
         service.leituraBufferedReader("textoPequeno", "Pequeno");
+        System.out.println("------------------Character Reader------------------------");
+        service.leituraCharacterReader("textoGrande", "Grande");
+        service.leituraCharacterReader("textoMedio", "Medio");
+        service.leituraCharacterReader("textoPequeno", "Pequeno");
+        System.out.println("------------------Byte Reader------------------------");
+        service.leituraByteReader("textoGrande", "Grande");
+        service.leituraByteReader("textoMedio", "Medio");
+        service.leituraByteReader("textoPequeno", "Pequeno");
+
     }
 }
--- src/Service.java
+++ src/Service.java
@@ -26,6 +26,58 @@ public class Service {
             acabou = System.currentTimeMillis();
         }
         double resultado = acabou - inicio;
-        System.out.println("Desempenho Scanner( " + tipoArquivo + "): " + resultado  + " milissegundos");
+        System.out.printf("Desempenho Scanner( %s): %s milissegundos%n", tipoArquivo, resultado);
+    }
+
+    public void leituraCharacterReader(String caminhoEntrada, String tipoArquivo) throws IOException {
+        double inicio = System.currentTimeMillis();
+        FileReader in = null;
+        FileWriter out = null;
+        double acabou = 0;
+        try {
+            in = new FileReader(caminhoEntrada);
+            out =  new FileWriter("testeSaida.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if(out != null) {
+                out.close();
+            }
+            acabou = System.currentTimeMillis();
+        }
+        double resultado = acabou - inicio;
+        System.out.printf("Desempenho Scanner( %s): %s milissegundos%n", tipoArquivo, resultado);
+    }
+
+    public void leituraByteReader(String caminhoEntrada, String tipoArquivo) throws IOException {
+        double inicio = System.currentTimeMillis();
+        InputStream inputStream =  null;
+        OutputStream outputStream = null;
+        double acabou = 0;
+        try {
+            inputStream = new FileInputStream(caminhoEntrada);
+            outputStream = new FileOutputStream("testeSaida.txt");
+            int c;
+
+            while ((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if(outputStream != null) {
+                outputStream.close();
+            }
+            acabou = System.currentTimeMillis();
+        }
+        double resultado = acabou - inicio;
+        System.out.printf("Desempenho Scanner( %s): %s milissegundos%n", tipoArquivo, resultado);
     }
 }
